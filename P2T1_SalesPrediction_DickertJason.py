2c0febf37eb42f1ca8cf2e378b0e8e781e04bf33
jpdickert <40347781+jpdickert@users.noreply.github.com>
2018-06-17 23:22:38 -0400

    Deleting

--- P2T1_SalesPrediction_DickertJason.py
+++
@@ -1,9 +0,0 @@
-# CTI-110
-# P2T1 - Sales Prediction
-# Jason Dickert
-# 07 June 2018
-#
-print('Enter your projected amount of total sales.')
-number = float(input("Projected amount of total sales: $"))
-print('Your projected amount of total sales is $', (number), '.')
-print('Your projected annual profit will be about $', (number*.23), '.')
